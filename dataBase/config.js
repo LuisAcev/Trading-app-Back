cec6942f674fa5cb1d40cc9cd65351bd7e382ef7
FELIPE <tecnomin3@gmail.com>
2025-03-20 20:41:03 -0500

    change mongoose timeOutMS to 30000

--- dataBase/config.js
+++ dataBase/config.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-export const dbConnection =async()=>{
-
-    try {
-        await mongoose.connect(process.env.MONGODB_CNN);
-        console.log("Data Base Online")
-        
-    } catch (error) {
-        console.log(error);
-        throw new Error("Data Base Error");
-    }
-
-}
+export const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CNN, {
+      connectTimeoutMS: 30000, // Espera hasta 30 segundos
+    });
+    console.log("Data Base Online");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Data Base Error");
+  }
+};
